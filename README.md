6ea5938c3f3e60502f8794ea573ac89c3f426555
qiaochao <qiaochao@icourt.cc>
2024-02-28 21:29:55 +0800

    feat: array

--- README.md
+++ README.md
@@ -6,6 +6,18 @@ use of c
 * clang、clangd、lld 都是用的llvm里的，可以使用brew安装llvm
 * ninja是按照git的里的方法安装的
 * cmake使用了官方的加载器加载源码安装的
+
+在M1芯片的Mac上使用VS Code编写C代码，你需要按照以下步骤操作：
+1. 如果你还没有安装VS Code，可以从其官方网站下载适用于Apple Silicon的版本。
+
+2. 打开VS Code，然后安装C/C++扩展。这可以通过VS Code的扩展商店来完成，你只需在商店中搜索“c++”，然后直接装pack
+
+3. 在VS Code的设置中，你需要将clang设定为默认的C/C++编译器。你可以在VS Code的设置中搜索“c++”，然后找到“C_Cpp > Default Compiler Path”设置项，将其设置为"/usr/bin/clang"，或者其他clang路径，可以在Default Compiler Path的setting.json中直接设置
+"C_Cpp.default.compilerPath": "/usr/bin/clang",
+"C_Cpp.default.cStandard": "c99"
+
+以上步骤完成后，你就可以开始在你的M1 Mac上通过VS Code和Xcode的clang编写C代码了。 
+
   
 ## run
 ```
@@ -35,5 +47,4 @@ executable.on('close', (code) => {
 });
 
 ```
-
 
--- index.c
+++ index.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <stdlib.h>  // lib，包含了malloc和free函数
+#include <stdio.h>   // io是输出相关
+
  
 /** 
  * 这里编译用的clang是xcode的，位置在/usr/bin/clang
@@ -8,22 +10,59 @@
  * 这也是它为什么快的原因
  */
 
+void updateValue(int *pValue){
+    *pValue = 20;
+}
+
 int main (){
  
     int var = 20; /* 实际变量的声明 */
  
+    // c语言基本数据类型
+    int num = 10;
+    char ch = 'a';
+    float f = 3.14f;
+    double d = 3.14;
+    printf("The int variable is: %d\n", num);     // 输出整型变量
+    printf("The char variable is: %c\n", ch);     // 输出字符型变量
+    printf("The float variable is: %.2f\n", f);   // 输出单精度浮点型变量，保留2位小数
+    printf("The double variable is: %.2lf\n", d); // 输出双精度浮点型变量，保留2位小数 
+
+    //基本数组 type array[size]
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    //----1. *的第一个作用，声明指针变量
     int *ip; /* 这里*是取值符，ip就是地址 */
  
     ip = &var; /*  &为取地址符 */
  
-    /* 输出var的地址 */
-    printf("Address of var variable: %p\n", &var );
- 
-    /* 输出指针变量中存储的地址 */
-    printf("Address stored in ip variable: %p\n", ip );
- 
-    /* 使用指针访问值 即用*取出地址中的值 */
+
+    /*----2. *的第二个作用，取出地址中的值 */
     printf("Value of *ip variable: %d\n", *ip );
+
+
+    //接着哟个动态数组说下*的其他作用
+    int* array;
+    int n, i;
+    n = 5;
+    //----3、*的第三个作用动态分配内存
+    array = (int*)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++) {
+        array[i] = i;
+    }
+    // 打印数组中的元素
+    for (i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    // 释放内存
+    free(array);
+
+
+    //----4、*的第四个作用，通过引用传参
+    int value = 10;
+    updateValue(&value);
+    printf("After: %d\n", value);
+
  
     return 0;
  
